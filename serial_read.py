3ccd9eaefff347e8bf0c2c1f864f6843ae1bdee9
Nick Chouard <nick.chouard@gmail.com>
2018-01-27 10:37:27 -0600

    reading serial data from raspberry pi

---
+++ serial_read.py
@@ -0,0 +1,12 @@
+import serial
+import struct
+from pythonosc import udp_client
+
+ser = serial.Serial('/dev/cu.usbserial')
+
+client = udp_client.SimpleUDPClient('127.0.0.1', 6969)
+
+while True:
+    data = struct.unpack('f', ser.read(4))
+    print(int(data[0]))
+    client.send_message('/theremin/midi', int(data[0]))
